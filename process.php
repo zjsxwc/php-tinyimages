21e6d20bb22ac6f2d427bcd51b3cfcdc834e7471
Wang Chao <watcher.hangzhou@foxmail.com>
2020-04-20 15:51:19 +0800

    Update process.php

--- process.php
+++ process.php
@@ -8,23 +8,34 @@ function qualityCompress($inputImageFullPath, $outputImageFullPath, $quality = 7
     if (!file_exists($inputImageFullPath)) {
         return false;
     }
+    if ($quality <= 1) {
+    	$quality = 1;
+    }
     @mkdir(dirname($outputImageFullPath), 0777, true);
     @unlink($outputImageFullPath);
     $output = [];
     $returnVar = -1;
     exec("convert -quality " . $quality . "% " . $inputImageFullPath . " " . $outputImageFullPath, $output, $returnVar);
     if ($returnVar != 0) {
+    	die("cannot process" . $inputImageFullPath);
         return false;
     }
     return $quality;
 }
 
+global $tryTimes;
+$tryTimes = 1;
+
 function compress($inputImageFullPath, $outputImageFullPath, $quality = 75)
 {
+	global $tryTimes;
     echo "start process $inputImageFullPath quality $quality\n";
     $quality = qualityCompress($inputImageFullPath, $outputImageFullPath, $quality);
     if ($quality) {
         $targetSize = 0.26 * 1024 * 1024;//<------------         here let the size of every image is less than 260 KByte
+        if ($tryTimes) {
+        	$targetSize = $targetSize * $tryTimes;
+        }
         $currentSize = filesize($outputImageFullPath);
         echo "current size $currentSize \n";
         echo "target size $targetSize \n";
@@ -32,8 +43,14 @@ function compress($inputImageFullPath, $outputImageFullPath, $quality = 75)
         if ($currentSize > $targetSize) {
             $newQuality = 1.0 * $quality * ($targetSize * 1.0 / $currentSize);
             $newQuality = intval(ceil($newQuality)) - 1;
+            if ($newQuality <= 1) {
+		    	$newQuality = 1;
+		    }
             echo "new quality $newQuality\n";
+            $tryTimes++;
             compress($inputImageFullPath, $outputImageFullPath, $newQuality);
+        } else {
+        	$tryTimes = 1;
         }
     }
 }
